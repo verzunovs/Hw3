e72676e7ee8b8c4b93dfba82828e41b27fe5f6a8
Serhii Verzunov <shvabroid1221@gmail.com>
2020-12-03 19:48:13 +0200

    Added class "PasaengerTaxi". Changed type of taxi.

---
+++ Enums/TypeOfTaxi.cs
@@ -0,0 +1,23 @@
+﻿namespace Leson3M2.Enums
+{
+    /// <summary>
+    /// Type of taxi in taxi station.
+    /// </summary>
+    public enum TypeOfTaxi
+    {
+        /// <summary>
+        /// It is taxi econom type. It is the cheapest in taxi station.
+        /// </summary>
+        Econom,
+
+        /// <summary>
+        /// It is taxi standart type. More comfortable than econom.
+        /// </summary>
+        Standart,
+
+        /// <summary>
+        /// It is taxi luxury type. Premium class car.
+        /// </summary>
+        Luxary,
+    }
+}
---
+++ Models/Сars/PassangerTaxi.cs
@@ -0,0 +1,9 @@
+﻿namespace Leson3M2.Models.Сars
+{
+    /// <summary>
+    /// Passanger taxi.
+    /// </summary>
+    public abstract class PassangerTaxi : Car
+    {
+    }
+}
