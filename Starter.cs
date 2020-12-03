a09eb011ea4357370ec4e453e9c1ed6f3bee001e
Serhii Verzunov <shvabroid1221@gmail.com>
2020-12-03 22:08:02 +0200

    Added class Run.

--- Starter.cs
+++ Starter.cs
@@ -9,5 +9,20 @@
     {
         private readonly ITaxiStationService taxiStationService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Starter"/> class.
+        /// </summary>
+        public Starter()
+        {
+            this.taxiStationService = new TaxiStationService();
+        }
+
+        /// <summary>
+        /// Method for run program.
+        /// </summary>
+        public void Run()
+        {
+            var taxiStation = this.taxiStationService.MakeTaxiStation();
+        }
     }
 }
